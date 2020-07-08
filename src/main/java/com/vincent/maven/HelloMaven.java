b643a6a05b013dab0d86981481de8893a3ae3eb8
wanglibiao <wang614235383@163.com>
2020-07-08 10:13:28 +0800

    hello maven

---
+++ src/main/java/com/vincent/maven/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.vincent.maven;
+
+/**
+ * @author : wanglibiao
+ * @date : 2020/7/8 - 10:12
+ */
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println("hello maven");
+    }
+
+}
